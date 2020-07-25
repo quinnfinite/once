f79c15d36fed55f2be8eb51a7a8f9637207cbcfc
Quinn Royston <quinn.royston@gmail.com>
2020-07-25 12:33:53 -0700

    updated config files

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,11 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.jsx',
+  watch: true,
+  mode: 'development',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public/dist'),
   },
   module: {
     rules:[{
