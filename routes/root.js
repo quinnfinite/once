a7f19338906c5ff70406e6c6ffbd9fc9d5c6f05d
Quinn Royston <quinn.royston@gmail.com>
2020-07-25 12:32:52 -0700

    updated server to serve static assets and parse params

--- routes/root.js
+++ routes/root.js
@@ -1,8 +1,14 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
 
 module.exports = router;
 
 router.get('/', (req, res)=> {
-  res.send('I am the root of all things')
+  res.sendFile('index')
+});
+
+
+router.get('/:id', (req, res)=> {
+  const {id} = req.params;
+  res.send(`Number: ${id}`);
 })
--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ require('dotenv').config()
 var express = require('express');
 var app = express();
 const path = require('path');
+express.json()
+
+app.use(express.static(path.join(__dirname, '../public')))
+
 const mountRoutes = require('../routes');
 mountRoutes(app);
-
 
 app.listen(process.env.PORT, (req, res) => {
   console.log(`Listening at http://localhost:${4000}`)
