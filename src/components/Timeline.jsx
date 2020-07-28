cf590c665102d37a21cbdf549940a05d39de50eb
Quinn Royston <quinn.royston@gmail.com>
2020-07-27 21:40:01 -0700

    updated timeline component

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -3,6 +3,7 @@ import Post from './Post.jsx'
 
 const Timeline = (props) => (
   <div>
+    <h4>Timeline</h4>
     {props.posts.map((post, key) => {
       return <Post data={post} key={key}/>
     })}
