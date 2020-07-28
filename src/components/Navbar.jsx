9702689a7250d249a27a2b19132fb378b254aa80
Quinn Royston <quinn.royston@gmail.com>
2020-07-27 17:30:08 -0700

    Added a navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const Navbar = (props) => (
-  <div>Navbar</div>
+  <AppBar position="static">
+    <Toolbar>
+      <IconButton edge="start" color="inherit" aria-label="menu">
+        <MenuIcon />
+      </IconButton>
+      <Typography variant="h6" >
+        Once
+      </Typography>
+    </Toolbar>
+  </AppBar>
 )
 
 export default Navbar;
